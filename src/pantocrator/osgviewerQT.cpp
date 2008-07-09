94acdfa8735f1a0c4314e5c7602d8fad8af99e2a
jtorresfabra <jtorresfabra@1043d9c5-a650-0410-8246-399b919c848a>
2008-07-09 12:02:47 +0000

    first executable working

---
+++ src/pantocrator/osgviewerQT.cpp
@@ -0,0 +1,114 @@
+/* OpenSceneGraph example, osganimate.
+*
+*  Permission is hereby granted, free of charge, to any person obtaining a copy
+*  of this software and associated documentation files (the "Software"), to deal
+*  in the Software without restriction, including without limitation the rights
+*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+*  copies of the Software, and to permit persons to whom the Software is
+*  furnished to do so, subject to the following conditions:
+*
+*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+*  THE SOFTWARE.
+*/
+
+#if USE_QT4
+
+    #include <QtGui/QApplication>
+#else
+
+    #include <qapplication.h>
+#endif
+
+#include <osg/ArgumentParser>
+
+#include <iostream>
+#include <osg/Node>
+
+#include <osgGA/TrackballManipulator>
+#include <osgGA/FlightManipulator>
+#include <osgGA/DriveManipulator>
+#include <osgGA/KeySwitchMatrixManipulator>
+#include <osgGA/StateSetManipulator>
+#include <osgGA/AnimationPathManipulator>
+#include <osgGA/TerrainManipulator>
+
+#include <osgDB/ReadFile>
+#include <osgParti/QOSGWidget.hpp>	
+#include <iostream>
+using namespace osgParti;
+
+
+void setupManipulatorAndHandler(osgViewer::View & viewer, osg::ArgumentParser & arguments)
+{
+    // set up the camera manipulators.
+    {
+        osg::ref_ptr<osgGA::KeySwitchMatrixManipulator> keyswitchManipulator = new osgGA::KeySwitchMatrixManipulator;
+
+        keyswitchManipulator->addMatrixManipulator( '1', "Trackball", new osgGA::TrackballManipulator() );
+        keyswitchManipulator->addMatrixManipulator( '2', "Flight", new osgGA::FlightManipulator() );
+        keyswitchManipulator->addMatrixManipulator( '3', "Drive", new osgGA::DriveManipulator() );
+        keyswitchManipulator->addMatrixManipulator( '4', "Terrain", new osgGA::TerrainManipulator() );
+
+        viewer.setCameraManipulator( keyswitchManipulator.get() );
+    }
+
+    // add the state manipulator
+    viewer.addEventHandler( new osgGA::StateSetManipulator(viewer.getCamera()->getOrCreateStateSet()) );
+    
+    // add the thread model handler
+    viewer.addEventHandler(new osgViewer::ThreadingHandler);
+
+    // add the window size toggle handler
+    viewer.addEventHandler(new osgViewer::WindowSizeHandler);
+        
+    // add the stats handler
+    viewer.addEventHandler(new osgViewer::StatsHandler);
+
+    // add the help handler
+    viewer.addEventHandler(new osgViewer::HelpHandler(arguments.getApplicationUsage()));
+}
+
+int main( int argc, char **argv )
+{
+    QApplication a( argc, argv );
+    
+    if (argc<2)
+    {
+        std::cout << argv[0] <<": requires filename argument." << std::endl;
+        return 1;
+    }
+
+    osg::ArgumentParser arguments(&argc, argv);
+
+     osg::ref_ptr<osg::Node> loadedModel = osgDB::readNodeFiles(arguments);
+    if (!loadedModel)
+    {
+        std::cout << arguments[0] <<": No data loaded." << std::endl;
+        return 1;
+    }
+
+    std::cout<<"Using QOSGWidget - QWidget + osgViewer creating the graphics context."<<std::endl;
+    
+
+        osg::ref_ptr<ViewerQOSG> viewerWindow(new ViewerQOSG);
+        viewerWindow->setGeometry(0,0,640,480);
+        
+        viewerWindow->setCameraManipulator(new osgGA::TrackballManipulator);
+        viewerWindow->setSceneData(loadedModel.get());
+
+        viewerWindow->show();
+
+        setupManipulatorAndHandler(*viewerWindow.get(), arguments);
+        
+        a.connect( &a, SIGNAL(lastWindowClosed()), &a, SLOT(quit()) );
+    
+        return a.exec();
+
+}
+
+/*EOF*/
